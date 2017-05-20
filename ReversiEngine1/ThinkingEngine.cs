60a78f007520081e7d81f24378cb0607ae198eb5
浩平 佐伯 <atihsa.mihs.ne.ho.da@gmail.com>
2017-05-20 10:34:06 +0900

    最初のコミット

---
+++ ReversiEngine1/ThinkingEngine.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThinkingEngineBase;
+using Reversi.Core;
+
+namespace ReversiEngine1
+{
+    public class ThinkingEngine : IThinkingEngine
+    {
+        public string Name
+        {
+            get
+            {
+                return "弱いエンジン";
+            }
+        }
+
+        public void SetTimeLimit(int milliSecond)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ReversiMove> Think(ReversiBoard board, StoneType player)
+        {
+            return await Task.Run(() =>
+            {
+                return board.SearchLegalMoves(player).First();
+            });
+        }
+    }
+}
